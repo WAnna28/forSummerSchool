eb656e56849e26c9f47bf4756185a418cfdeb405
WAnna28 <69531966+WAnna28@users.noreply.github.com>
2020-08-13 14:42:34 +0400

    Add files via upload

---
+++ current.cs
@@ -0,0 +1,19 @@
+using System;
+ 
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+			Console.ReadLine();
+        }
+		
+	 static void AnnaTEST()
+	 {
+            Console.WriteLine("Hello World FROM ANNA TEST Point!");
+			Console.ReadLine();
+	 }
+    }
+}
